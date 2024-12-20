094fc934ecc8b9d331ff9d8f9126db7be4d8f832
laurapuerto82 <laura.puerto.martin@gmail.com>
2024-12-20 17:46:12 +0100

    Added run and idle animator states

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,20 +2,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Components
     private Rigidbody2D m_rigidbody2D;
     private GatherInput m_gatherInput;
     private Transform m_transform;
+    private Animator m_animator;
 
+    // Values
     [SerializeField] private float speed;
     private int direction = 1;
+
+    // Animator values
+    private int idSpeed = Animator.StringToHash("Speed");
 
     private void Awake()
     {
         m_rigidbody2D = GetComponent<Rigidbody2D>();
         m_gatherInput = GetComponent<GatherInput>();
         m_transform = GetComponent<Transform>();
-    }    
-    
+        m_animator = GetComponent<Animator>();        
+    }
+
+    private void Update()
+    {
+        SetAnimatorValues();
+    }
+
+    private void SetAnimatorValues()
+    {
+        m_animator.SetFloat(idSpeed, Mathf.Abs(m_rigidbody2D.linearVelocityX));
+    }
+
     private void FixedUpdate()
     {
         Move();
